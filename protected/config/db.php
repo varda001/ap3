82588f72485e0b024baf400681dbd5004ce4744f
root <muhammad-alif-v@protonmail.com>
2024-12-27 03:05:06 +0000

    im so dumb

--- protected/config/db.php
+++ protected/config/db.php
@@ -1,10 +1,10 @@
 <?php
 
 return array(
-    'connectionString' => 'mysql:host=bunda_mysql;port=3306;dbname=ahadpos3',
+    'connectionString' => '',
     'emulatePrepare' => true,
     'username' => 'root',
-    'password' => 'efab6937db92f93b9a3a',
+    'password' => '',
     'charset' => 'utf8',
     'enableParamLogging' => true,
 );
